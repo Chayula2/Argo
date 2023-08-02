b92db151fb10321851a5d35c6fdebb3122974dd4
Chayula2 <49929379+Chayula2@users.noreply.github.com>
2023-08-02 11:31:05 +0200

    Banner+Icons
    banner and icons upload

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-<?php
+<?php ?>
+
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <!-- Informazioni generali -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Argo Robot</title>
+
+    <!-- Metadati per il SEO -->
+    <meta name="description" content="Argo can easily meet your needs. A smart companion robot equipped with sophisticated sensors and AI algorithms to keep your home safe">
+    <meta name="keywords" content="argo,robot,domestic, assistant, AI, smart">
+    <meta name="author" content="Argo">
+    <meta name="robots" content="index, follow">
+
+    <!-- Metadati per i social media -->
+    <meta property="og:title" content="Argo">
+    <meta property="og:description" content="Argo can easily meet your needs. A smart companion robot equipped with sophisticated sensors and AI algorithms to keep your home safe">
+
+    <!-- Collegamenti al CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+
+    <!-- Collegamenti al favicon  todo-->
+<!--    <link rel="icon" href="" type="image/x-icon">-->
+<!--    <link rel="apple-touch-icon" href="">-->
+
+
+</head>
+<body>
+<!-- Contenuto della pagina -->
+</body>
+</html>
