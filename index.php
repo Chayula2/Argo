63b9858144b18e80d8e7d6293f1cb29805384e6a
Chayula2 <49929379+Chayula2@users.noreply.github.com>
2023-08-27 17:50:02 +0200

    part#1
    primo grande upload

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
-<?php include_once "theme/head.php";?>
+<?php include_once "theme/head.php"; ?>
 <body>
 
 <!--  menu-->
 <nav class="navbar navbar-expand-lg navbar-dark justify-content-center text-center" style="background-color: #000000">
     <div class="container">
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse w-100" id="navbarNav">
@@ -14,7 +15,7 @@
                     <a class="nav-link argo-text" href="#">DISCOVER</a>
                 </li>
                 <li class=" mx-4">
-                    <a class="nav-link argo-text" href="#">FEATURES</a>
+                    <a class="nav-link argo-text" href="#">BUY</a>
                 </li>
                 <li class="">
                     <a class="" href="#" style="margin-left: 70px;margin-right: 70px">
@@ -22,10 +23,10 @@
                     </a>
                 </li>
                 <li class=" mx-4">
-                    <a class="nav-link argo-text" href="#">GET YOUR</a>
+                    <a class="nav-link argo-text" href="#">ABOUT US</a>
                 </li>
                 <li class=" mx-4">
-                    <a class="nav-link argo-text" href="#">ABOUT US</a>
+                    <a class="nav-link argo-text" href="#">PLATFORM</a>
                 </li>
             </ul>
         </div>
@@ -40,9 +41,13 @@
     <div class="row justify-content-center m-auto">
         <div class="col-12 px-0 justify-content-center m-auto">
             <div class="owl-carousel-wrapper">
-                <!-- Carosello --><div class="argo-title" style="position: absolute;color: white;z-index: 999;margin-top: 15%;font-weight: 800;font-size: 128px">A NEW WAY<br>
+                <!-- Carosello -->
+                <div class="argo-title"
+                     style="position: absolute;color: white;z-index: 999;margin-top: 15%;font-weight: 800;font-size: 128px">
+                    A NEW WAY<br>
                     TO LIVE<br>
-                    AT HOME</div>
+                    AT HOME
+                </div>
                 <div class="owl-carousel owl-theme">
                     <div class="owl-carousel-item"><img src="media/banners/banner-1.png" alt="Argo Banner 1"></div>
                     <div class="owl-carousel-item"><img src="media/banners/banner-2.png" alt="Argo Banner 2"></div>
@@ -55,14 +60,279 @@
     <div class="row" style="margin-top: 100px">
         <div class="col-lg-6 col-md-6 col-sm-10 justify-content-center m-auto text-center">
             <span class="argo-title font-weight-bold">THE SMART COMPANION ROBOT<br> FOR YOUR FAMILY</span>
-            <p class="argo-text"  style="margin-top: 100px">Have you ever been worried about your elderly relatives living alone, your children at home or your naughty pets? Are you always looking for new ways to have fun with your family?
+            <p class="argo-text" style="margin-top: 100px">Have you ever been worried about your elderly relatives
+                living alone, your children at home or your naughty pets? Are you always looking for new ways to have
+                fun with your family?
 
-                Argo can easily meet your needs. A smart companion robot equipped with sophisticated sensors and AI algorithms to keep your home safe and entertain your family. Vocal assistant, video-surveillance and autonomous navigation. This is just a taste of what this robot can do. Argo is the ideal companion whether you are at home or not.</p>
+                Argo can easily meet your needs. A smart companion robot equipped with sophisticated sensors and AI
+                algorithms to keep your home safe and entertain your family. Vocal assistant, video-surveillance and
+                autonomous navigation. This is just a taste of what this robot can do. Argo is the ideal companion
+                whether you are at home or not.</p>
         </div>
     </div>
+
+    <!--foto faded-->
+    <img class="w-100 " style="margin-top: 200px" src="media/banners/faded-main-2.png">
+    <div class="row" style="margin-top: -200px">
+        <div class="col-12 px-0 text-center">
+            <span class="text-center argo-big-title">SMART</span><br>
+            <span class="argo-under-title">AN AI POWERED HOMEBOT</span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="justify-content-center m-auto col-lg-6 col-md-6 col-sm-12">
+            <div class="row" style="margin-top: 150px">
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/route-1.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/chip-2.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/battery-2.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/voice-2.png" height="120px" class="d-block m-auto">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--    blocco con logica-->
+    <div class="row justify-content-center mt-5">
+        <div class="col-lg-6 col-md-6 col-sm-12 px-0 text-center">
+            <span class="text-center font-weight-bold argo-title">Autonomous Navigation</span><br><br>
+            <span class="argo-text mt-4">Exploiting Lidar technology, Argo is capable of mapping your house. Once the map is ready, it will be able to move autonomously around your home, avoiding any obstacle it may encounter along its way.</span>
+        </div>
+    </div>
+
+    <!--foto faded 2-->
+    <img class="w-100 " style="margin-top: 200px" src="media/banners/faded-main-2.png">
+    <div class="row" style="margin-top: -200px">
+        <div class="col-12 px-0 text-center">
+            <span class="text-center argo-big-title">SMART</span><br>
+            <span class="argo-under-title">AN AI POWERED HOMEBOT</span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="justify-content-center m-auto col-lg-6 col-md-6 col-sm-12">
+            <div class="row" style="margin-top: 150px">
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/route-1.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/chip-2.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/battery-2.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/voice-2.png" height="120px" class="d-block m-auto">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--    blocco con logica-->
+    <div class="row justify-content-center mt-5">
+        <div class="col-lg-6 col-md-6 col-sm-12 px-0 text-center">
+            <span class="text-center font-weight-bold argo-title">Autonomous Navigation</span><br><br>
+            <span class="argo-text mt-4">Exploiting Lidar technology, Argo is capable of mapping your house. Once the map is ready, it will be able to move autonomously around your home, avoiding any obstacle it may encounter along its way.</span>
+        </div>
+    </div>
+
+    <!--foto faded-->
+    <img class="w-100 " style="margin-top: 200px" src="media/banners/faded-main-2.png">
+    <div class="row" style="margin-top: -200px">
+        <div class="col-12 px-0 text-center">
+            <span class="text-center argo-big-title">SMART</span><br>
+            <span class="argo-under-title">AN AI POWERED HOMEBOT</span>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="justify-content-center m-auto col-lg-6 col-md-6 col-sm-12">
+            <div class="row" style="margin-top: 150px">
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/route-1.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/chip-2.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/battery-2.png" height="120px" class="d-block m-auto">
+                </div>
+                <div class="col-3 justify-content-center m-auto">
+                    <img src="media/icons/voice-2.png" height="120px" class="d-block m-auto">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--    blocco con logica-->
+    <div class="row justify-content-center mt-5">
+        <div class="col-lg-6 col-md-6 col-sm-12 px-0 text-center">
+            <span class="text-center font-weight-bold argo-title">Autonomous Navigation</span><br><br>
+            <span class="argo-text mt-4">Exploiting Lidar technology, Argo is capable of mapping your house. Once the map is ready, it will be able to move autonomously around your home, avoiding any obstacle it may encounter along its way.</span>
+        </div>
+    </div>
+
+
+    <!--    wasnt enough-->
+    <div class="row" style="margin-top: 400px">
+        <div class="col-12 px-0 text-center">
+            <span class="text-center argo-big-title">WASN'T ENOUGH?</span><br>
+            <span class="argo-under-title">Argo is plenty of features!<br>Discover them all here below</span>
+        </div>
+    </div>
+
+
+    <div class="row justify-content-center m-auto" style="margin-top: 50px">
+        <div class="col-lg-8 col-md-6 col-sm-12">
+            <div class="row text-center" style="margin-top: 100px">
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+
+            </div>
+
+            <div class="row text-center" style="margin-top: 100px">
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+
+            </div>
+
+            <div class="row text-center" style="margin-top: 100px">
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+                <div class="col-4">
+                    <img src="media/icons/telecamera.png" class="img-fluid d-block m-auto" style="height: 200px">
+                    <span class="font-weight-bold argo-title">FEATURE</span>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row" style="margin-top: 400px">
+        <div class="col-12 px-0 text-center">
+            <span class="text-center argo-big-title">HOW IT'S MADE</span><br>
+        </div>
+
+        <div class="col-6 mt-5 text-center">
+            <div class="font-weight-bold argo-title" style="padding-top: 70px;">
+                Technical component
+            </div>
+            <span class="argo-text text-center"
+                  style="padding-top: 70px;">Breve spiegazione per ogni singolo componente</span>
+
+        </div>
+        <div class="col-6 mt-5"><img src="media/argo/Group%2056.png" class="m-auto d-block"></div>
+    </div>
+
+
+    <!--COMING SOON-->
+    <div class="row" style="margin-top: 400px">
+        <div class="col-12 px-0 text-center">
+            <span class="text-center argo-big-title">COMING SOON</span><br>
+            <hr class="" width="20%">
+            <span class="argo-text text-center" style="padding-top: 70px;">Argo is getting ready for launch.<br>
+        Stay tuned for exciting updates!</span>
+
+            <input name="emailSubscription" type="text" class="form-control justify-content-center m-auto"
+                   placeholder="EMAIL"
+                   style="border-radius: 15px;margin-top:90px !important;width: 600px;font-size: 20px;">
+            <input type="button" class="form-control justify-content-center m-auto text-white" value="SIGN ME UP"
+                   style="border-radius: 15px;margin-top:30px !important;width: 200px;font-size: 20px;background-color: black">
+        </div>
+
+    </div>
+
+    <!--COMING SOON-->
+    <div class="row" style="margin-top: 400px">
+        <div class="col-12 px-0 text-center">
+            <span class="text-center argo-big-title">MEET THE TEAM</span><br>
+
+        </div>
+        <div class="col-12  text-center" style="margin-top: 80px">
+            <span class="argo-text" style="padding-top: 70px;"><b>“How can be possible in 2022 we still do not have a personal robot in our homes?”</b><br>
+            <br>
+            This is the question of which made us start thinking about Argo!<br>
+            <br>
+            ...and who better than 3 guys who built driver less cars, drones, rockets and Mars Rovers<br> to build the future smart companion robot for your home?</span>
+        </div>
+
+        <div class="col-12" style="margin-top: 200px;margin-bottom: 200px">
+            <iframe class="d-block m-auto" width="560" height="315"
+                    src="https://www.youtube.com/embed/0Ftq9X3epLU?si=7ryyRPWEGgzDevsC" title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
+        </div>
+
+        <div class="col-lg-6 col-md-6 col-sm-12  text-center d-block m-auto ">
+            <span class="argo-text">
+                Tortor dignissim convallis aenean et tortor at risus. Nisl rhoncus mattis rhoncus urna neque viverra justo nec ultrices. Consequat id porta nibh venenatis cras sed felis eget velit.
+<br><br>
+Suspendisse potenti nullam ac tortor vitae purus faucibus ornare. Tellus mauris a diam maecenas sed. Sed euismod nisi porta lorem mollis aliquam ut. Donec ultrices tincidunt arcu non sodales. Imperdiet dui accumsan sit amet nulla facilisi.
+<br><br>
+Quis auctor elit sed vulputate mi sit amet mauris. Aliquet risus feugiat in ante. Neque gravida in fermentum et sollicitudin ac.
+            </span>
+        </div>
+
+    </div>
+
+
+    <div class="row" style="margin-top: 200px">
+        <div class="col-lg-8 col-md-8 col-sm-12 justify-content-center m-auto">
+            <div class="row">
+                <div class="col-lg-4 col-lg-4 col-sm-12 text-center" style="margin-top: 50px;">
+                    <img src="media/Rectangle%204239.png" class="m-auto d-block">
+                    <br>
+                    <span class="argo-title font-weight-bold" style="font-size: 30px">LEONARDO BERTELLI</span>
+                </div>
+                <div class="col-lg-4 col-lg-4 col-sm-12" style="margin-top: 50px;">
+                    <img src="media/Rectangle%204239.png" class="m-auto d-block">
+                </div>
+                <div class="col-lg-4 col-lg-4 col-sm-12" style="margin-top: 50px;">
+                    <img src="media/Rectangle%204239.png" class="m-auto d-block">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div style="margin-bottom: 900px"></div>
 </div>
 
 </body>
 
-<?php include_once "theme/foot.php";?>
+<?php include_once "theme/foot.php"; ?>
 
